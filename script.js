9ea667f4d06b0680d09e1550c73cb7b45be5f27e
Sean Yap <syap0317@gmail.com>
2021-03-14 21:54:04 -0400

    add comment structure to script

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-'use strict';
+"use strict";
+
+// grab all elements you need to change/update
+// total score
+// current score
+// dice pic
+
+// grab all elements you need to add event listeners
+// new game
+// roll dice
+// hold
+
+// store CURRENT and TOTAL score for 2 players
+// keep track of current player
+
+// event listener for when roll dice
+
+// event listener for when hold
+
+// function to switch player
